86acf2453e7ea8c0872c46fe1bdcfc842522f168
Ram Babu <ram.kottapally@gmail.com>
2021-10-29 17:00:15 +0530

    Not using constants currently

--- constants.py
+++ constants.py
@@ -5,3 +5,5 @@ API_KEY = os.environ.get('TELEGRAM_BOT_TOKEN')
 QUOTE_URL = 'https://api.quotable.io/random'
 
 CHOICES: dict = {'0': ['Go back to Menu'], '1': ['Who are you?'], '2': ['Random Quote'], '3': ['Know Time']}
+
+d = {'1': '1️⃣', '2': '2️⃣', '3': '3️⃣', '4': '4️⃣', '0': '0️⃣'}
